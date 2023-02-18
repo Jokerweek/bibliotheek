52615e0837db2c5423c0be8043cb1a0d68b73562
stelemme <stef.lemmens@ugent.be>
2023-02-18 13:23:22 +0100

    literature page

--- src/components/CardLiterature/index.js
+++ src/components/CardLiterature/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import {React, useState } from "react";
 
 // Material ui Imports
 import {
@@ -9,9 +9,31 @@ import {
   Typography,
   CardActionArea,
   Chip,
+  CardActions,
+  IconButton,
+  Collapse,
 } from "@mui/material";
+import { styled } from '@mui/material/styles';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
 
 export default function CardLiterature(props) {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
   return (
     <Grid container justifyContent="center">
       <Card sx={{ width: 300 }}>
@@ -37,11 +59,11 @@ export default function CardLiterature(props) {
             <Typography
               color="text.secondary"
               sx={{
-                minHeight: 48,
+                minHeight: 24,
                 display: "-webkit-box",
                 overflow: "hidden",
                 WebkitBoxOrient: "vertical",
-                WebkitLineClamp: 2,
+                WebkitLineClamp: 1,
               }}
             >
               {props.authors}
@@ -49,14 +71,28 @@ export default function CardLiterature(props) {
             
           </CardContent>
         </CardActionArea>
-        <CardContent>
-          {props.tags.map((element) => {
-            return(
-              <Chip label={element} clickable variant="outlined" sx={{margin : '3px'}} />
-            )
-          })}
-        </CardContent>
+        <CardActions>
+          <ExpandMore
+            expand={expanded}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </ExpandMore>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            {props.tags.map((element) => {
+              return(
+                <Chip label={element} clickable variant="outlined" sx={{margin : '3px'}} />
+              )
+            })}
+          </CardContent>
+        </Collapse>
       </Card>
     </Grid>
   );
 }
+
+
